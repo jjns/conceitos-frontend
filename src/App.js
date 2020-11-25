5b4088416d5fcc1ec62afbdf5834d86ded795117
Neto <jjns.januario@gmail.com>
2020-11-25 12:34:27 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpg';
 
 import Header from './components/Header';
 
 function App() {
-    const [ projects, setProjects ] = useState(['Desenvolvimento App', 'Front-end web']);
+    // useState retorna duas variáveis:
+    // 1ª variável inicial
+    // 2ª função para atualizar a variável
+    const [ projects, setProjects ] = useState([]);
 
-    function handleAddProject() {
-        setProjects([...projects, `Novo projeto ${Date.now()}`]);
-    }
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleAddProject() {
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: 'José Januário'
+        });
+
+        const project = response.data;
+
+        setProjects([... projects, project]);
+    };
 
     return (
         // Fragment (Container que envolve para não afetar na estilização)
         <>
-            <Header title="Projects"/>
-
-            <img src={backgroundImage} />
+            <Header title="Projects:"/>
 
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type='button' onClick={handleAddProject}>Adicionar projeto</button>
